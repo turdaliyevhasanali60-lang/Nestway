49d1f5ea39bac1d89e0b58dcd6868318e2cbb9f5
Hasanali Turdaliyev <turdaliyevhasanali60@gmail.com>
2026-05-20 12:16:05 +0500

    Add /favicon.ico route to ensure browsers load favicon reliably

--- nestway/urls.py
+++ nestway/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.png', permanent=True)),
 ]
 
 if settings.DEBUG:
